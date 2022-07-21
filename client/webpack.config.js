0aabf111b5fff082da2bf0af51664b36265204d1
Hans Reichgelt <hreichgelt06@gmail.com>
2022-07-20 23:17:49 -0400

    adding loaders and plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,8 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const WebpackPwaManifest = require("webpack-pwa-manifest");
-const path = require("path");
-const { InjectManifest } = require("workbox-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -22,13 +23,29 @@ module.exports = () => {
         title: "Client Server",
         template: "./index.html",
       }),
+      new MiniCssExtractPlugin(),
+
       new InjectManifest({
         swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
+        swDest: 'service-worker.js',
 
       }),
       new WebpackPwaManifest({
-
+        // Create a manifest.json: from 26 stu manifest
+        name: "Text Editor",
+        short_name: "JATE", 
+        fingerprints: false, 
+        inject: true,
+        description: "Just Another Text Editor",
+        background_color: "blue",
+        start_url: './', 
+        public_path: './', 
+        icons:[
+          {
+            src: path.resolve('./assets/images/icon.png'), 
+            sizes:[96, 128, 192, 256, 384, 512],  
+          },
+        ]
       }),
     ],
 
